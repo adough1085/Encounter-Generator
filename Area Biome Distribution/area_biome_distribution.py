c13aef3aabf76b36448d1de1c950eedeb6ed32d8
confusingProgram <doandy@sheridancollege.ca>
2026-02-04 12:59:29 -0500

    Added list of areas to calculate biome distribution

--- Area Biome Distribution/area_biome_distribution.py
+++ Area Biome Distribution/area_biome_distribution.py
@@ -5,7 +5,10 @@ import os
 class Area:
     def __init__(self, number, name):
         self.number = number # Number corresponds to numbers in image files, which is roughly chronological order in-game
-        self.name = name # Written like "Alfornada Cavern"
+        proto_name_arr = name.split(" ")
+        for token in proto_name_arr:
+            self.name += token.title() + " "
+        self.name = self.name[:len(self.name)-1] # Written like "Alfornada Cavern", ensuring that it comes out to the right caps regardless of initial input
         self.snake_case_name = "" # Written like "alfornada_cavern"
         self.image_name = ""
         self.biomes = []
@@ -85,3 +88,70 @@ class Area:
         self.calculate_distribution()
         self.csv()
 
+list = []
+cabo_poco = Area(1, "Cabo Poco")
+poco_path = Area(2, "Poco Path")
+inlet_grotto = Area(3, "Inlet Grotto")
+south_province_area_one = Area(4, "South Province (Area One)")
+south_province_area_two = Area(5, "South Province (Area Two)")
+south_province_area_three = Area(6, "South Province (Area Three)")
+west_province_area_one = Area(7, "West Province (Area One)")
+south_paldean_sea = Area(8, "South Paldean Sea")
+south_province_area_four = Area(9, "South Province (Area Four)")
+south_province_area_five = Area(10, "South Province (Area Five)")
+east_province_area_one = Area(11, "East Province (Area One)")
+east_province_area_two = Area(12, "East Province (Area Two)")
+east_province_area_three = Area(13, "East Province (Area Three)")
+west_province_area_two = Area(14, "West Province (Area Two)")
+tagtree_thicket = Area(15, "Tagtree Thicket")
+west_province_area_three = Area(16, "West Province (Area Three)")
+east_paldean_sea = Area(17, "East Paldean Sea")
+west_paldean_sea = Area(18, "West Paldean Sea")
+glaseado_mountain = Area(19, "Glaseado Mountain")
+alfornada_cavern = Area(20, "Alfornada Cavern")
+south_province_area_six = Area(21, "South Province (Area Six)")
+asado_desert = Area(22, "Asado Desert")
+north_province_area_three = Area(23, "North Province (Area Three)")
+north_province_area_two = Area(24, "North Province (Area Two)")
+north_province_area_one = Area(25, "North Province (Area One)")
+north_paldean_sea = Area(26, "North Paldean Sea")
+casseroya_lake = Area(27, "Casseroya Lake")
+socarrat_trail = Area(28, "Socarrat Trail")
+dalizapa_passage = Area(29, "Dalizapa Passage")
+pokemon_league = Area(30, "Pokemon League")
+great_crater_of_paldea = Area(31, "Great Crater of Paldea")
+
+list.append(cabo_poco)
+list.append(poco_path)
+list.append(inlet_grotto)
+list.append(south_province_area_one)
+list.append(south_province_area_two)
+list.append(south_province_area_three)
+list.append(west_province_area_one)
+list.append(south_paldean_sea)
+list.append(south_province_area_four)
+list.append(south_province_area_five)
+list.append(east_province_area_one)
+list.append(east_province_area_two)
+list.append(east_province_area_three)
+list.append(west_province_area_two)
+list.append(tagtree_thicket)
+list.append(west_province_area_three)
+list.append(east_paldean_sea)
+list.append(west_paldean_sea)
+list.append(glaseado_mountain)
+list.append(alfornada_cavern)
+list.append(south_province_area_six)
+list.append(asado_desert)
+list.append(north_province_area_three)
+list.append(north_province_area_two)
+list.append(north_province_area_one)
+list.append(north_paldean_sea)
+list.append(casseroya_lake)
+list.append(socarrat_trail)
+list.append(dalizapa_passage)
+list.append(pokemon_league)
+list.append(great_crater_of_paldea)
+
+for area in list:
+    area.run()
