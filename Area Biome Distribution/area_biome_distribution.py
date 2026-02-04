f82b79427e6957a37fa1df49a65654879de237fb
confusingProgram <doandy@sheridancollege.ca>
2026-02-04 12:10:35 -0500

    Redesigned count() method in area_biome_distribution.Area such that it requires no parameters when called at runtime

--- Area Biome Distribution/area_biome_distribution.py
+++ Area Biome Distribution/area_biome_distribution.py
@@ -3,9 +3,11 @@ from pixel_count.pixel_count import count_colour_pixels as ccp
 import os
 
 class Area:
-    def __init__(self, name):
-        self.name = name
-        self.snake_case_name = "" # Snake Case
+    def __init__(self, number, name):
+        self.number = number # Number corresponds to numbers in image files, which is roughly chronological order in-game
+        self.name = name # Written like "Alfornada Cavern"
+        self.snake_case_name = "" # Written like "alfornada_cavern"
+        self.image_name = ""
         self.biomes = []
         self.biome_pixels = {}
         self.distribution = {}
@@ -18,22 +20,54 @@ class Area:
             area_name += token + "_"
         self.snake_case_name = area_name[:len(area_name)-1]
 
-    def count(self, image_path):
-        # Expects image path to be something like 3Paldea_Inlet_Grotto_Map_Cave.png
-        arr = image_path.split("_")
-        biome_arr = arr[len(arr)-1].split(".")
-        biome = biome_arr[0]
-        if biome not in self.biomes:
-            self.biomes.append(biome)
-            bp = ccp(image_path, self.target_colour_rgb)
-            self.biome_pixels[biome] = bp
-            self.pixel_total += bp
-        self.biomes.sort()
+        name_pascal_underscore = self.name.replace(" ", "_")
+        i_name = f"{str(self.number)}Paldea_{name_pascal_underscore}"
+        self.image_name = i_name
+
+        
+
+    def count(self):
+        # For every biome, check if it exists in the region
+        image_folder = Path("../images/")
+        area_folder = image_folder / self.image_name
+        biomes_list = ["Bamboo_Forest","Beach","Cave","Desert","Flower","Forest","Lake","Mine","Mountain",
+                       "Ocean","Olive","Prairie","Riverside","Rocky_Area","Ruins","Snowfield","Swamp","Town"]
+        
+        for biome in biomes_list:
+            file_name = f"{area_folder}_Map_{biome}.png"
+            file_path = area_folder / file_name
+            # Test if the region exists, if yes, add biome to existing biomes in region, and count pixels.
+            try:
+                with open(file_path, 'r') as f:
+                    pass # Don't actually need to try reading anything; this is to check if the file exists
+                biome_name = biome.replace("_", " ")
+                if biome_name not in self.biomes:
+                    self.biomes.append(biome_name)
+                    b_pixels = ccp(file_path, self.target_colour_rgb)
+                    self.biome_pixels[biome_name] = b_pixels
+                    self.pixel_total += b_pixels
+            except FileNotFoundError:
+                pass # Do nothing
 
     def calculate_distribution(self):
+        # For every biome, access their pixels within the biome_pixels dictionary and compare it to pixel_total.
+        # Write the percentage within the distribution dictionary.
         for biome in self.biomes:
             percentage = self.biome_pixels[biome] / self.pixel_total
             self.distribution[biome] = percentage
 
     def csv(self):
-        pass
+        subdirectory_path = Path("distributions")
+        file_name = self.snake_case_name + ".csv"
+        file_path = subdirectory_path / file_name
+        subdirectory_path.mkdir(parents=True, exist_ok=True)
+
+        # Overwriting in case there is pre-existing text
+        with open(file_path,"w") as f1:
+            f1.write("Biome,Percentage\n")
+
+        # Writing each biome and distribution
+        with open(file_path, "a") as f1:
+            for biome in self.biomes:
+                line = f"{biome},{self.distribution[biome]}\n"
+                f1.write(line)
