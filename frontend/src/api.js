d261738854b3f74b913d3d9d943fbf2bf75ce381
Andy Do <andydo835@gmail.com>
2026-02-09 09:00:00 +0000

    Working baseURL for testing (has the /api suffix)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with the base URL
 const api = axios.create({
-    baseURL: "https://3.137.101.123" // baseURL required for deployment
+    baseURL: "https://3.137.101.123/api" // baseURL required for deployment
     //baseURL: "http://localhost:8000" // baseURL required for testing
 })
 
