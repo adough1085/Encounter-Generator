7559c4e16ccf104f49192d5bb4de41e3aab9ffec
Andy Do <andydo835@gmail.com>
2026-02-09 08:20:50 +0000

    Frontend changes needed for deployment; api.js has the baseURL for deployment, vite.config.js builds deployment-ready files.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,13 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+  },
   server: {
-    host: '0.0.0.0', // This makes Vite listen on all network interfaces
+    //host: '0.0.0.0', // This makes Vite listen on all network interfaces
+    host: '0.0.0.0', // 
     port: 5173, // Optional: specify the port (default is 5173)
-    // Optional: for HMR to work correctly in some Docker/WSL setups
-    hmr: {
-      host: 'localhost', 
-      protocol: 'ws',
-    },
   },
 })
