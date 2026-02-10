5ac6606519c898dfe4209271a9aa2a5a54832540
confusingProgram <doandy@sheridancollege.ca>
2026-02-10 15:07:26 -0500

    Changed title of tab header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,21 @@ import './App.css';
 import PokemonList from './components/Pokemons';
 
 const App = () => {
+
+  useEffect(() => {
+    // Set the document title when the component mounts or updates
+    document.title = "Pokémon Scarlet and Violet Pokémon Picker Tool"
+    
+    // Optional: Reset the title when the component unmounts
+    return () => {
+      // document.title = "Original App Title"; 
+    };
+  }, []); // The empty dependency array [] ensures this runs once when mounted
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pokémon Scarlet and Violet Random Generator and Distribution Calculator</h1>
+        <h1>Pokémon Scarlet and Violet Pokémon Picker Tool</h1>
       </header>
       <main>
         <PokemonList />
